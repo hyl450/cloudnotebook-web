a614ffdb62f1e397424e2d0a18466eeb4101998f
huangyl_oup <huangyl_oup@si-tech.com.cn>
2023-03-06 09:12:37 +0800

    add by huangyl 登录注册控件图标

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -49,6 +49,15 @@ module.exports = {
       //     ]
       //   }
       // },
+      // add by huangyl
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        include: [resolve('src/icons')],
+        options: {
+          symbolId: 'icon-[name]'
+        }
+      },
       {
         test: /\.js$/,
         loader: 'babel-loader',
@@ -57,6 +66,7 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
+        exclude: resolve('src/icons'),
         options: {
           limit: 10000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
