b8a684d488a24cd2d89822cb281974385814591c
huangyl_oup <huangyl_oup@si-tech.com.cn>
2023-03-10 22:21:16 +0800

    add by huangyl 添加域名部署，外网穿透访问虚拟机Web应用

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,6 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"http://localhost:8088"'
-  BASE_API: '"http://192.168.150.128:8089/cloudnote-admin"'
+  // BASE_API: '"http://192.168.150.128:8089/cloudnote-admin"'
+  BASE_API: '"http://e94rsb.natappfree.cc/cloudnote-admin"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,6 @@
 module.exports = {
   NODE_ENV: '"production"',
   // BASE_API: '"http://localhost:8088"'
-  BASE_API: '"http://192.168.150.128:8089/cloudnote-admin"'
+  // BASE_API: '"http://192.168.150.128:8089/cloudnote-admin"'
+  BASE_API: '"http://e94rsb.natappfree.cc/cloudnote-admin"'
 }
