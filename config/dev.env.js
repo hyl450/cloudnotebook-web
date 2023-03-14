5dede459b61ee61188141093cd83e30ddde229ad
huangyl_oup <huangyl_oup@si-tech.com.cn>
2023-03-14 16:08:48 +0800

    add by huangyl vue前端本地调试相关配置

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,6 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // BASE_API: '"http://localhost:8088"'
+  BASE_API: '"http://192.168.150.128:8088"'
   // BASE_API: '"http://192.168.150.128:8089/cloudnote-admin"'
-  BASE_API: '"http://92ym7x.natappfree.cc/cloudnote-admin"'
+  // BASE_API: '"http://92ym7x.natappfree.cc/cloudnote-admin"'
 })
